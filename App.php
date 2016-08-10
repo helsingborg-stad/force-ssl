ff6d24b7ed599199a51c1303b54a53c7913dd82b
Sebastian Thulin <sebastian.thulin@gmail.com>
2016-08-10 13:22:25 +0200

    Creating plugin

---
+++ App.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace ForceSSL;
+
+class App
+{
+    public function __construct()
+    {
+        add_action('admin_init', array($this, 'backend'));
+        add_action('template_redirect', array($this, 'frontend'));
+        add_action('all_plugins', array($this, 'preventMultisiteActivation'));
+    }
+
+    public function frontend()
+    {
+        if (!is_ssl()) {
+            wp_redirect('https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'], 301);
+            exit();
+        }
+    }
+
+    public function backend()
+    {
+        define('FORCE_SSL_ADMIN', true);
+    }
+
+    public function preventMultisiteActivation()
+    {
+        global $current_screen;
+
+        if ($current_screen->is_network) {
+            unset($all['akismet/akismet.php']);
+        }
+        return $all;
+    }
+}
---
+++ force-ssl.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * Plugin Name: Force SSL
+ * Plugin URI: -
+ * Description: Force users to use ssl
+ * Version: 1.0
+ * Author: Sebastian Thulin
+ * Author URI: -
+ * Text domain: force-ssl
+ *
+ * Copyright (C) 2016
+ */
+
+//Location definition
+define('FORCE_SSL_PATH', plugin_dir_path(__FILE__));
+
+//The good stuff
+require FORCE_SSL_PATH . 'App.php';
+
+//Run it!
+new ForceSSL\App();
